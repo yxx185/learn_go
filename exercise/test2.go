61340eadd98456421f579f88defecda4d6fd766f
RunningSnail <yanxinxin185@163.com>
2022-04-04 00:02:21 +0800

    test2

---
+++ exercise/test2.go
@@ -0,0 +1,30 @@
+package main
+import (
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"errors"
+)
+func main() {
+	group := new(errgroup.Group)
+	nums := []int{-1, 0, 1}
+
+	for _, num := range nums {
+		tempNum := num // 子协程中若直接访问num，则可能是同一个变量，所以要用临时变量
+
+		// 子协程
+		group.Go(func() error {
+			if tempNum < 0 {
+				return  errors.New("tempNum < 0 !!!")
+			}
+			fmt.Println("tempNum:",tempNum)
+			return nil
+		})
+	}
+
+	// 捕获err
+	if err := group.Wait(); err != nil {
+		fmt.Println("Get errors: ", err)
+	}else {
+		fmt.Println("Get all num successfully!")
+	}
+}
